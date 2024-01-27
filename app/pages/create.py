496593e573b9335450dc5e0ccae0641e075f238e
Arsal Abrar <aarsal359@gmail.com>
2024-01-27 12:56:11 -0500

    added menu to all pages

--- app/pages/create.py
+++ app/pages/create.py
@@ -2,4 +2,6 @@ from taipy.gui import Gui, navigate
 import cohere
 
 create_pg = '''
+<|menu|label=Menu|lov={page_names}|on_action=on_menu|>
+Create
 '''
--- app/pages/home.py
+++ app/pages/home.py
@@ -1,4 +1,6 @@
 from taipy.gui import Gui, navigate
 
 home_pg = '''
+<|menu|label=Menu|lov={page_names}|on_action=on_menu|>
+Home
 '''
