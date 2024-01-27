5d7d64f0c2618e074115ff516170383bb65d591a
Arsal Abrar <aarsal359@gmail.com>
2024-01-27 17:20:57 -0500

    commit

--- app/pages/create.py
+++ app/pages/create.py
@@ -2,11 +2,28 @@ from taipy.gui import Gui, navigate, Markdown
 import cohere
 import numpy as np
 
-#cohere component
 co = cohere.Client('90lG7PeJDEJgf4pWS0ObCcS57x97XaUwh15g2U7Z')
 
-response = co.chat(
-  chat_history=[
+path_upload=""
+dt=""
+memories=""
+img=""
+
+def on_change(state, var_name, var_val):
+  if var_name == "memories":
+    state.memories = var_val
+    
+  if var_name == "path_upload":
+    state.img = var_val
+
+def button_pressed(state):
+  
+  # error msg
+  if state.memories == "":
+    notify()
+
+  response = co.chat(
+    chat_history=[
     {
         "role": 
         "USER", "message": 
@@ -19,24 +36,23 @@ response = co.chat(
         "On this day, you and your friends went on an adventure. It began with shared ice cream, leaving stains on your face that left you all giggling. Then turning into rollerblading gliding through the park. You stumbled and fell, and your friend after some laugher lifted you up."
         }
   ],
-  message="Make a less superficial story with emotion with the following memories: I played video games with my friends all night and we tried over and over again just to win by the time we won we decided to meet up and eat a late night snack at mcdonalds "
+    message="Tell a story using these image descriptions in choronological order: number each description and output with image number"+combined_description
+
 )
-print(response)
-
-
-path_upload=""
 
 create_pg = '''
 <|menu|label=Menu|lov={page_names}|on_action=on_menu|>
 
 <|{"../memento.svg"}|image|width=0.1|>
+<|toggle|theme|>
 
 <|text-center|
 <|{path_upload}|file_selector|extensions=.png,.jpg|label=Upload image|>
-<|{path_upload}|image|>
 <|{dt}|not with_time|date|>
-<|{memories}|label=Memory associated with image?|multiline=True|lines= 5|action_keys="Enter"|input|>
+<|{memories}|label=Memory associated with image?|multiline=True|lines= 5|action_keys="Enter"|on_change=get_memories|input|>
 
-<|{response.message}|text|>
+<|{path_upload}|image|>
+
+<|{"Submit"}|button|on_action=button_pressed|>
 >
 '''
