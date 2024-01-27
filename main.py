e38a2f7c4e93e79837d980d374810ee14243ec50
Arsal Abrar <aarsal359@gmail.com>
2024-01-26 23:02:57 -0500

    virtual env

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
-from taipy import Config
+# import taipy as tp
+# from taipy import Config, Core, Gui
+
+# ################################################################
+# #            Configure application                             #
+# ################################################################
+# def build_message(name):
+#     return f"Hello {name}!"
+
+
+# # A first data node configuration to model an input name.
+# input_name_data_node_cfg = Config.configure_data_node(id="input_name")
+# # A second data node configuration to model the message to display.
+# message_data_node_cfg = Config.configure_data_node(id="message")
+# # A task configuration to model the build_message function.
+# build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
+# # The scenario configuration represents the whole execution graph.
+# scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
+
+# ################################################################
+# #            Design graphical interface                        #
+# ################################################################
+
+# input_name = "Taipy"
+# message = None
+
+
+# def submit_scenario(state):
+#     state.scenario.input_name.write(state.input_name)
+#     state.scenario.submit()
+#     state.message = scenario.message.read()
+
+# page = """
+# Name: <|{input_name}|input|>
+
+# <|submit|button|on_action=submit_scenario|>
+
+# Message: <|{message}|text|>
+# """
+
+# if __name__ == "__main__":
+#     ################################################################
+#     #            Instantiate and run Core service                  #
+#     ################################################################
+#     Core().run()
+
+#     ################################################################
+#     #            Manage scenarios and data nodes                   #
+#     ################################################################
+#     scenario = tp.create_scenario(scenario_cfg)
+
+#     ################################################################
+#     #            Instantiate and run Gui service                   #
+#     ################################################################
+
+#     Gui(page).run()
+
+import cohere
+co = cohere.Client('AjLh0IFke3oYIcUtLQjLsfy3ESoZ6m8tlWbt7CWu')
+
+corpus = ['food is good', 'games are better', 'sports are the best']
+query = 'what is the best'
+
+results = co.rerank(query=query, documents=corpus, model='rerank-multilingual-v2.0')
+print(results)
