1960705d642ccddb97fdb011c0d7c0f799e70062
xhans <hanwinhtun334@gmail.com>
2024-01-27 13:35:00 -0500

    Update create.py

--- app/pages/create.py
+++ app/pages/create.py
@@ -1,6 +1,7 @@
 from taipy.gui import Gui, navigate, Markdown
 import cohere
 import numpy as np
+from PIL import Image
 
 #cohere component
 co = cohere.Client('90lG7PeJDEJgf4pWS0ObCcS57x97XaUwh15g2U7Z')
@@ -26,7 +27,14 @@ print(response)
 memories =""
 
 create_pg = '''
-<|{""}|label= add image|drop_message= "drop here to upload"|file_selector|>
+
+<|{path_upload}|file_selector|on_action=save_to_db|extensions=.png,.jpg|label=Upload image|>
 <|{datetime}|not with_time|date|>
 <|{""}|label=What memory is associated with this image?|multiline=True|lines= 5|action_keys="Enter"|input|>
 '''
+
+def save_to_db(state):
+    image = Image.open(state.path_upload)
+    print(image)
+    
+
